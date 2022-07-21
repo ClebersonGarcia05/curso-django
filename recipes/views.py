d8024bbf56ab90733f08106aa89f399acbab7e37
Cleberson Garcia Nascimento <cleberson.garcia051299@gmail.com>
2022-07-21 15:01:28 -0300

    Home finalizado

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 
 
 def home(request):
-    return render(request, 'recipes/pages/home.html')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': [make_recipe() for _ in range(10)],
+    })
 
 
 def recipes(request, id):
-    return render(request, 'recipes/pages/recipe-detail.html')
+    return render(request, 'recipes/pages/recipe-detail.html', context={
+        'recipe': make_recipe(),
+        'is_datail_page': True,
+    })
