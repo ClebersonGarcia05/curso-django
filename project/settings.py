e52fd9643af8a371d7edce79454ddabf3c8a1d65
Cleberson Garcia Nascimento <cleberson.garcia051299@gmail.com>
2022-07-29 17:01:54 -0300

    Tirando erros

--- project/settings.py
+++ project/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-woxzoyrmzw7b50d-!p-p5wcc5xr0v$d2o0k4c8k6e!mn_)rfek'
+SECRET_KEY = 'django-insecure-woxzoyrmzw7b50d-!p-p5wcc5xr0v$d2o0k4c8k6e!mn_)rfek'  # noqa: E501
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = []  # type: ignore
 
 
 # Application definition
@@ -89,16 +89,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',  # noqa: E501
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',  # noqa: E501
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',  # noqa: E501
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',  # noqa: E501
     },
 ]
 
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-""" STATICFILES_DIRS = [ colocar pastas de arquivos estáticos sem ser a padrão (static)] """
+""" STATICFILES_DIRS = [ colocar pastas de arquivos estáticos sem ser a padrão (static)] """  # noqa: E501
 STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
